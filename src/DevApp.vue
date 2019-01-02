49adaf49d82b98d5a124e8a7fe604e98d3f4766c
anoChick <anoinchick@gmail.com>
2019-01-02 21:13:51 +0900

    Setup for development

---
+++ src/DevApp.vue
@@ -0,0 +1,18 @@
+<template lang='pug'>
+.app-container
+  revue-form
+</template>
+
+<script>
+import Vue from 'vue'
+import RevueForm from './RevueForm.vue'
+export default Vue.extend({
+  components: {
+    RevueForm
+  }
+})
+</script>
+
+<style lang='sass' scoped>
+
+</style>
