3ed063e5d43ef55dc42d5048233fc3710cfcda27
anoChick <anoinchick@gmail.com>
2019-01-06 01:24:23 +0900

    Add createUser function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,19 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp()
+
+const db = admin.firestore()
+db.settings({
+  timestampsInSnapshots: true
+})
+
+exports.createUser = functions.auth.user().onCreate((userRecord, context) => {
+  const docRef = db.collection('users').doc(userRecord.uid)
+  const currentUserRef =  db.collection('users').doc(userRecord.uid)
+  return docRef.set({
+    uid: userRecord.uid,
+    photoURL: userRecord.photoURL,
+    displayName: userRecord.displayName
+  })
+})
