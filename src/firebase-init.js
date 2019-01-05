39a3d9ad6bbb210764b0e48c56fbf26ab9152823
anoChick <anoinchick@gmail.com>
2019-01-06 02:30:04 +0900

    Add firebase-init

---
+++ src/firebase-init.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+const config = {
+  apiKey: "API_KEY",
+  authDomain: "AUTH_DOMAIN",
+  databaseURL: "DATABASE_URL",
+  projectId: "PROJECT_ID"
+}
+if (!firebase.apps.length) {
+firebase.initializeApp(config)
+}
+export const auth = firebase.auth()
+export const db = firebase.firestore()
+db.settings({timestampsInSnapshots: true})
